fc1dc2fecc429bddeaf8ee9cb71c40166e40383c
sumanth <108932595+sumanth-reddy13@users.noreply.github.com>
2023-02-21 17:16:12 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,17 +1,19 @@
 package com.example.supplychainmanagement;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.*;          // step 1 - import the package
+
+// Establishing the connection b/w Java Application and Database.
 
 public class DatabaseConnection {
 
-    String SQLURL = "jdbc:mysql://localhost:3306/supplychain?useSSL=false";
-    String user = "root";
+    String SQLURL = "jdbc:mysql://localhost:3306/supplychain?useSSL=false";          // URL of the Database.
+    String user = "root";       
     String password = "sumanth13";
-    Connection con = null;
+    Connection con = null;           // Here Connection is an interface. We use Class "DriverManager" to get create the obj of Connection. In line 16, getConnection is                                      // the static method.
     DatabaseConnection()
     {
         try {
-            con = DriverManager.getConnection(SQLURL, user, password);
+            con = DriverManager.getConnection(SQLURL, user, password);        // Step 2 - Establishing the connection.
         }
         catch(Exception e){
             e.printStackTrace();
@@ -25,8 +27,8 @@ public class DatabaseConnection {
     {
         ResultSet res = null;
         try {
-            Statement statement = con.createStatement();
-            res = statement.executeQuery(query);
+            Statement statement = con.createStatement();                   // Step 4 - Create the statement
+            res = statement.executeQuery(query);                           // Step 5 - Execute the Query.
             return res;
         }
         catch(Exception e){
